1b5bca5704a5d67c8d4320733aebd4c339553243
Markus Seidl <mark@markus-seidl.de>
2016-05-22 11:28:55 +0200

    Working version to download audible books from their homepage.

--- audible_dl/main.py
+++ audible_dl/main.py
@@ -2,14 +2,33 @@
 # pip install -U selenium
 
 # Import the Selenium 2 namespace (aka "webdriver")
+import keyring
+from selenium.common.exceptions import ElementNotVisibleException
 from selenium import webdriver
 from selenium.webdriver.support.select import Select
 import os
 import glob
 import time
 
+# Configuration part
 OUTPUT_DIR = "/Users/msei/Downloads/Audible/"
+AUDIBLE_USER = keyring.get_password("audible_dl", "user")
+AUDIBLE_PASS = keyring.get_password("audible_dl", "pass")
+# /Configuration part
 
+##########
+
+# Handle initial keyring setup
+if AUDIBLE_USER is None:
+    AUDIBLE_USER = raw_input("Audible username:")
+
+if AUDIBLE_PASS is None:
+    AUDIBLE_PASS = raw_input("Audible password:")
+
+keyring.set_password("audible_dl", "user", AUDIBLE_USER)
+keyring.set_password("audible_dl", "pass", AUDIBLE_PASS)
+
+# Downloads
 profile = webdriver.FirefoxProfile()
 profile.set_preference("browser.download.folderList", 2)
 profile.set_preference("browser.download.manager.showWhenStarting", "false")
@@ -27,8 +46,8 @@ def login(d):
     input_email = d.find_element_by_id("ap_email")
     input_password = d.find_element_by_id("ap_password")
 
-    input_email.send_keys("seidl.markus@googlemail.com")
-    input_password.send_keys("PASSWORD")
+    input_email.send_keys(AUDIBLE_USER)
+    input_password.send_keys(AUDIBLE_PASS)
 
     d.find_element_by_id("signInSubmit").click()
 
@@ -49,7 +68,7 @@ driver.get("https://www.audible.de/lib")
 select_lib_range = Select(driver.find_element_by_id("adbl_time_filter"))
 select_lib_range.select_by_index(5)
 
-time.sleep(1)
+time.sleep(4)
 
 for i in range(0, 50):
     download_buttons = driver.find_elements_by_class_name("adbl-download-it")
@@ -61,7 +80,11 @@ for i in range(0, 50):
             continue
 
         print("Download " + name)
-        db.click()
+        try:
+            db.click()
+        except ElementNotVisibleException:
+            print("Failed (ElementNotVisibleException)")
+            continue
 
         part_file = exists_part_file(OUTPUT_DIR)
         while not exists_part_file(OUTPUT_DIR) is None:
@@ -73,7 +96,7 @@ for i in range(0, 50):
     next_link = next_links[0]
     next_link.click()
 
-    time.sleep(1)
+    time.sleep(2)
 
 
 
